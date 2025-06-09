72689f81e60020cc3774bd90beb74606420d8651
Deepak <108412773+deepakbehera11@users.noreply.github.com>
2025-06-09 20:53:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Flask sample application!!"
+    return "HI this is Deepak, and it's a Flask sample application"
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True,host='0.0.0.0',port=port)
